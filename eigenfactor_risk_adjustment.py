39569b69f5bfb510d2497e0e8b60b0b7cb688f3b
YuNan <nanyu201411@gmail.com>
2018-09-26 11:34:25 +0800

    Initial commit

---
+++ eigenfactor_risk_adjustment.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+class EigenFactorAdjust(object):
+    def __init__(self, origin_matrix):
+        self.origin = origin_matrix
+
+    def simulation_factor_return(self):
+        pass
+
+    def calculate_simulated_volatility(self):
+        pass
+
+    def scale_volatility(self):
+        pass
+
+    def adjust_eigenmatrix(self):
+        pass
+
+    def adjust_covariance_matrix(self):
+        pass
